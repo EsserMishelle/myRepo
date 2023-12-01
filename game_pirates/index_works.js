18f34ddfee20d146723acbafa896a66f8578f53f
M Esser <essermishelle@gmail.com>
2023-11-30 22:00:26 -0500

    pirate game flip unflip radio button

--- game_pirates/index_works.js
+++ game_pirates/index_works.js
@@ -3,8 +3,7 @@ let cards = [];
 // let firstCard, secondCard;
 // let lockBoard = false;
 let score;
-let currentPlayer = {name: '', color: '', value: '', bombs: 0, treasure: 0, scores: 100};
-
+let currentPlayer;
 
 document.querySelector(".score").textContent = score;
 //credit: memory game javascript academy
@@ -12,7 +11,6 @@ fetch("./data/cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
-    console.log("loaded: ", cards); 
     shuffleCards();
     generateCards();
   });
@@ -31,71 +29,30 @@ function shuffleCards() {
 }
 
 function generateCards() {
-
-// resetBoard();
-
-for (let i=0; i< 6; i++) {
-  for (let j=0; j <3; j++){
-    console.log(`The row is now ${i}, col is ${j}`)
+    //   resetBoard();
+  for (let card of cards) {
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
-    cardElement.setAttribute("data-name", cards[i*3+j].name);
+    cardElement.setAttribute("data-name", card.name);
     cardElement.innerHTML = `
-      <div class ="front">
-      <img class = "front-image" src=${cards[i*3+j].image} />
-      </div>
-      <div class ="back"></div>
+    <div class ="front">
+      <img class = "front-image" src=${card.image} />
+    </div>
+    <div class ="back"></div>
     `;
     gridContainer.appendChild(cardElement);
     cardElement.addEventListener("click", flipCard);
-    cardElement.addEventListener('drag', handleCardElDrag);
-    cardElement.dataset.row = i;
-    cardElement.dataset.col = j;
-     }
   }
 }
-
-function handleCardElDrag(event) {
-    const draggedCard = event.target;
-    const row = draggedCard.dataset.row;
-    const col = draggedCard.dataset.cel;
-//if no player is selected from the toggle, remind the player to click on the toggle
-  if (!currentPlayer) {
-    alert('Please select a pirate before dragg your piece to the desired spot');
-    return;
-  }
-
-//check to see if the cell is empty
-  if (draggedCard.classList.contains('occupied')) {
-        //place the player's piece
-        const pieceColor = players[currentPlayer].color;
-        draggedCard.classList.add('occupied', pieceColor);
-  }    
-}
-//   for (let card of cards) {
-//     const cardElement = document.createElement("div");
-//     cardElement.classList.add("card");
-//     cardElement.setAttribute("data-name", card.name);
-//     cardElement.innerHTML = 
-//     <div class ="front">
-//       <img class = "front-image" src=${card.image} />
-//     </div>
-//     <div class ="back"></div>
-//     `;
-//     gridContainer.appendChild(cardElement);
-//     cardElement.addEventListener("click", flipCard);
-//   }
-// }
 // cardEl = document.querySelector("div");
 class Pirate{
-    constructor(name, color, treasures, bombs, scores, value){
+    constructor(name, color, treasures, bombs, scores){
         
         this.name = name;
         this.color = color;
-        this.treasures =0;
+        this.treasure =0;
         this.bombs=0;
         this.scores=100;
-        this.value= value;
         // this.flipCard= this.playCard.bind(this)
     }
     playCard(cardEl) {
@@ -117,15 +74,9 @@ class Pirate{
 //             cardEl.classList.remove('flipped');
 //         }, 1000);
 //     };
-// const pirates =[
-//   {name: 'Red Beard', color: 'red', value: 'opt-1'},
-//   {name: 'Green John Silver', color: 'green', value: 'opt-2'}
-// ];
-
-
 const pirates =[
- new Pirate('Red Beard', 'red', 0,0,100, 'opt-1'),
- new Pirate('Green John Silver', 'green', 0,0,100, 'opt-2')
+    {name: 'Red Beard', color: 'red', value: 'opt-1'},
+    {name: 'Green John Silver', color: 'green', value: 'opt-2'}
 ];
 
 // currentPlayer =document.querySelector('input[name="player-type"]:checked');
@@ -144,20 +95,19 @@ function flipCard() {
     alert('Please select a pirate before flipping a card');
     return;
   }
-  const selectedPirate= pirates.find((pirate)=>pirate.value === currentPlayer.value);
+  const selectedPirate = pirates.find((pirate)=>pirate.value === currentPlayer.value);
 //   selectedPirate.playCard(this);
   this.classList.add("flipped");
-  score = currentPlayer.scores;
-  console.log(currentPlayer)
-  if ((this.dataset.name ==='bomb1') || (this.dataset.name ==='bomb2') ) {
-      selectedPirate.scores = selectedPirate.scores-100;
-    
-    alert(`${selectedPirate.name} just lost 100 scores. It is ${selectedPirate.scores}`);   
+    score = currentPlayer.scores;
+    if ((this.dataset.name ==='bomb1') || (this.dataset.name ==='bomb2') ) {
+    currentPlayer.scores-=100;
+    alert(`${currentPlayer.scores}`)
+    alert(`${selectedPirate.name} just lost 100 scores`);   
     } else if ((this.dataset.name=== 'blue-treasure') || (this.dataset.name=== 'red-treasure') || (this.dataset.name=== 'yellow-treasure')) {
-        selectedPirate.treasures+=1;
-    alert(`Aha, found a treasure! ${selectedPirate.name} now has ${selectedPirate.treasures} treasure(s)! `);   
+        currentPlayer.treasures+=1;
+    alert(`Aha, found a treasure! ${selectedPirate.name} now has ${currentPlayer.treasures} treasure(s)! `);   
     }
-  setTimeout(() => {
+    setTimeout(() => {
         this.classList.remove('flipped');
         
         // resetBoard();
@@ -196,23 +146,19 @@ function unflipCards() {
   }, 1000);
 }
 
-function resetBoard() {
-  shuffleCards();
-  score = 0;
-  document.querySelector('.score').textContent = score;
-  gridContainer.innerHTML = '';
-  generateCards();
-  // currentPlayer =0;
-}
+// function resetBoard() {
+//   firstCard = null;
+//   secondCard = null;
+//   lockBoard = false;
+// }
 
 function restart() {
-  resetBoard();
+//   resetBoard();
   shuffleCards();
   score = 0;
   document.querySelector('.score').textContent = score;
   gridContainer.innerHTML = '';
   generateCards();
-  // currentPlayer =0;
 }
   // lockBoard = false;
   // setTimeout(() => {
