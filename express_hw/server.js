a2a08721a939468fe14e0137af1348d87dc88e41
M Esser <essermishelle@gmail.com>
2024-01-23 12:32:40 -0500

    Tip Calculator express application created

--- express_hw/server.js
+++ express_hw/server.js
@@ -18,3 +18,16 @@ app.get("/greeting/:name", (req, res) => {
   console.log(req.params);
   res.send("Wow, Hello There, " + req.params.name + " Good to see you!");
 });
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const { total, tipPercentage } = req.params;
+  console.log(req.params);
+
+  const totalAmount = parseFloat(total);
+  const tipPercentageValue = parseFloat(tipPercentage);
+
+  const tip = (totalAmount * tipPercentageValue) / 100;
+  res.send(
+    `The tip of total bill of ${totalAmount} with the tipping percentage ${tipPercentageValue}% is $${tip}`
+  );
+});
