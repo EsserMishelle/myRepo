ccf912a281b351dfcc6f38cbd086af0ffbd52e47
M Esser <essermishelle@gmail.com>
2024-01-23 12:58:20 -0500

    Magic 8 Ball express app created

--- express_hw/server.js
+++ express_hw/server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3020;
+// const magic8balls = require("./model/magic8balls");
+
+const magic8balls = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-//redner, redirect, json, status are most neeed
 
-// const greeting=[
-
-// ]
 app.get("/greeting", (req, res) => {
   res.send("Hello stranger");
 });
@@ -30,4 +50,14 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
   res.send(
     `The tip of total bill of ${totalAmount} with the tipping percentage ${tipPercentageValue}% is $${tip}`
   );
+});
+
+app.get("/magic/:question", (req, res) => {
+  const question = req.params.question.replace(/%20/g, " "); //got this from chat
+
+  const ranNum = Math.floor(Math.random() * magic8balls.length);
+  const answer = magic8balls[ranNum];
+
+  // Send the magic 8 ball response between html <h1> tags
+  res.send(`<h2>${question}</h3><h3>${answer}</h3>`);
 });
