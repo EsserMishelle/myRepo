3e4f99d7f8be42cdf7593b7d1be853df5a023164
M Esser <essermishelle@gmail.com>
2024-01-29 13:59:25 -0500

    My server is set up and running

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pokemon = require("./models/pokemon");
 
 // const vegetables = require("./models/vegetables.js");
 const app = express();
@@ -8,6 +9,10 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
+});
+
+app.get("/pokemon/", (req, res) => {
+  res.send(pokemon);
 });
 
 // app.get("/fruits/:indexOfFruitsArray", (req, res) => {
